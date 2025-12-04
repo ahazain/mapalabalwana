00aaca9f200dfff204866190f4a0b5137ff217ca
ahazain <ahmadfariddd5@gmail.com>
2025-12-04 22:55:06 +0700

    fix: final import casing for Config

--- mapalabalwana/src/pages/guest/NewsPortalPage.jsx
+++ mapalabalwana/src/pages/guest/NewsPortalPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import api from "../../services/Api";
-import { IMAGE_URL } from "../../services/Config";
+import { IMAGE_URL } from "../../services/config";
 
 /* --- 1. ICON COMPONENTS --- */
 const Icon = ({ path, className }) => (
