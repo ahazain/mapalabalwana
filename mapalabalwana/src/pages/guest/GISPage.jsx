8b7ba70131b00e5e61c16cfcd7a99c7258a75c4b
ahazain <ahmadfariddd5@gmail.com>
2025-12-04 22:36:07 +0700

    fix: final import casing for Config

--- mapalabalwana/src/pages/guest/GISPage.jsx
+++ mapalabalwana/src/pages/guest/GISPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import api from "../../services/Api";
-import { IMAGE_URL } from "../../services/Config";
+import { IMAGE_URL } from "../../services/config";
 
 /* --- 1. ICON COMPONENTS --- */
 const Icon = ({ path, className }) => (
