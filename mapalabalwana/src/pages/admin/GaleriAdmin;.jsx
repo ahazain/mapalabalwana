335f6d4db9b790db1e30fb17ea7096dce307d2dc
ahazain <ahmadfariddd5@gmail.com>
2025-12-04 22:14:19 +0700

    fix: final API rename and config import casing

--- mapalabalwana/src/pages/admin/GaleriAdmin;.jsx
+++ mapalabalwana/src/pages/admin/GaleriAdmin;.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import api from "../../services/api";
+import api from "../../services/Api";
 import { IMAGE_URL } from "../../services/Config";
 
 /* --- 1. ICON COMPONENTS --- */
